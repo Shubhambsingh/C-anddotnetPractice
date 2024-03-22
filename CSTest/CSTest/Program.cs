bd43b0be8241de066fb5c1afa75c7935aaf9db4d
Shubhambsingh <114708537+Shubhambsingh@users.noreply.github.com>
2024-03-22 00:20:32 -0400

    Add files via upload
    Simple Calculator Code

---
+++ CSTest/CSTest/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the first number");
+double firstnumber= Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter the Second number");
+double secondnumber = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter the operator ( + , - , * , / ): ");
+string Myoperator = Console.ReadLine();
+if(Myoperator.Length>1)
+{
+    Console.WriteLine("Enter a valid operator ( + , - , * , / ): ");
+    Myoperator = Console.ReadLine();
+}
+if (Myoperator == "+")
+{
+    Console.WriteLine(firstnumber + secondnumber);
+}
+if (Myoperator == "-")
+{
+    Console.WriteLine(firstnumber - secondnumber);
+}
+if (Myoperator == "*")
+{
+    Console.WriteLine(firstnumber * secondnumber);
+}
+if (Myoperator == "/")
+{
+    Console.WriteLine(firstnumber / secondnumber);
+    
+}
+Console.ReadLine();
